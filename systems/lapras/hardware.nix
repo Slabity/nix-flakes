f8b83044de24c18fbf11a494fe37eef13572bd72
Tyler Slabinski <tslabinski@slabity.net>
2022-12-16 16:55:46 -0500

    Update lapras

--- systems/lapras/hardware.nix
+++ systems/lapras/hardware.nix
@@ -11,35 +11,35 @@ with lib;
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" = {
-    device = "/dev/disk/by-partuuid/c1cfd094-9551-41cc-9587-18962f06c80d";
+    device = "/dev/disk/by-uuid/a535b9aa-97c2-4127-a1fe-0367fb375e87";
     fsType = "btrfs";
     options = [ "noatime,compress=zstd:1,ssd,space_cache=v2,subvol=system" ];
   };
 
   fileSystems."/nix" = {
-    device = "/dev/disk/by-partuuid/c1cfd094-9551-41cc-9587-18962f06c80d";
+    device = "/dev/disk/by-uuid/a535b9aa-97c2-4127-a1fe-0367fb375e87";
     fsType = "btrfs";
     options = [ "noatime,compress=zstd:1,ssd,space_cache=v2,subvol=nix" ];
   };
 
   fileSystems."/home" = {
-    device = "/dev/disk/by-partuuid/c1cfd094-9551-41cc-9587-18962f06c80d";
+    device = "/dev/disk/by-uuid/a535b9aa-97c2-4127-a1fe-0367fb375e87";
     fsType = "btrfs";
     options = [ "noatime,compress=zstd:1,ssd,space_cache=v2,subvol=data" ];
   };
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/7D47-9617";
+    device = "/dev/disk/by-uuid/AD68-C583";
     fsType = "vfat";
   };
 
-  swapDevices = [ { device = "/dev/disk/by-uuid/74793295-df8e-4373-906f-aeecf4bf35ff"; } ];
+  swapDevices = [ { device = "/dev/disk/by-uuid/b46c39f6-8b40-4aae-bbea-0267f895a9ce"; } ];
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.video.hidpi.enable = lib.mkDefault true;
