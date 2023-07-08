99dd89b00590e19361a25fbded3cd28050b6b58e
Tyler Slabinski <tslabinski@slabity.net>
2023-07-08 14:51:16 -0400

    Fix sway integration with xdg

--- modules/home-manager/workstation/default.nix
+++ modules/home-manager/workstation/default.nix
@@ -21,45 +21,6 @@ let
     bright.cyan = "#9AC1E4";
     bright.white = "#EAD8FF";
   };
-
-  # Below scripts taken from `https://nixos.wiki/wiki/Sway`
-
-  # bash script to let dbus know about important env variables and
-  # propagate them to relevent services run at the end of sway config
-  # see
-  # https://github.com/emersion/xdg-desktop-portal-wlr/wiki/"It-doesn't-work"-Troubleshooting-Checklist
-  # note: this is pretty much the same as  /etc/sway/config.d/nixos.conf but also restarts
-  # some user services to make sure they have the correct environment variables
-  dbus-sway-environment = pkgs.writeTextFile {
-    name = "dbus-sway-environment";
-    destination = "/bin/dbus-sway-environment";
-    executable = true;
-
-    text = ''
-      dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
-      systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
-      systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
-    '';
-  };
-
-  # currently, there is some friction between sway and gtk:
-  # https://github.com/swaywm/sway/wiki/GTK-3-settings-on-Wayland
-  # the suggested way to set gtk settings is with gsettings
-  # for gsettings to work, we need to tell it where the schemas are
-  # using the XDG_DATA_DIR environment variable
-  # run at the end of sway config
-  configure-gtk = pkgs.writeTextFile {
-    name = "configure-gtk";
-    destination = "/bin/configure-gtk";
-    executable = true;
-    text = let
-      schema = pkgs.gsettings-desktop-schemas;
-      datadir = "${schema}/share/gsettings-schemas/${schema.name}";
-    in ''
-      export XDG_DATA_DIRS=$XDG_DATA_DIRS:${datadir}
-      gsettings set org.gnome.desktop.interface gtk-theme 'Arc-Dark'
-    '';
-  };
 in
 {
   imports = [
@@ -177,8 +138,7 @@ in
           "${modifier}+Shift+r" = "restart";
         };
         startup = [
-          { command = "${dbus-sway-environment}/bin/dbus-sway-environment"; }
-          { command = "${configure-gtk}/bin/configure-gtk"; always = true; }
+          { command = "dbus-update-activation-environment --systemd --all"; }
         ];
       };
     };
