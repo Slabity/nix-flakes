41dcc35d2cfa68c8bcb4cb5066d72180d3323e4a
Tyler Slabinski <tslabinski@slabity.net>
2023-09-29 19:49:46 -0400

    Update rotom

--- systems/rotom/hardware.nix
+++ systems/rotom/hardware.nix
@@ -1,24 +1,10 @@
 { config, lib, pkgs, flake, ... }:
 {
   imports = [
-    flake.nixos-hardware.nixosModules.raspberry-pi-4
+    "${flake.nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
   ];
 
   system.stateVersion = "23.11";
-
   hardware.enableRedistributableFirmware = true;
-
-  boot.initrd.availableKernelModules = [ "xhci_pci" ];
-  boot.initrd.kernelModules = [ ];
-
-  boot.kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
-  boot.kernelModules = [ ];
-  boot.extraModulePackages = [ ];
-
-  /*fileSystems."/" = {
-    device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
-    fsType = "ext4";
-  };*/
-
-  powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
+  nixpkgs.config.allowUnsupportedSystem = true;
 }
