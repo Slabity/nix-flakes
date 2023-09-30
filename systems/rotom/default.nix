a66e4844aed6d9fb10127337c2c113e8877451ce
Tyler Slabinski <tslabinski@slabity.net>
2023-09-29 21:04:14 -0400

    Remove public key

--- systems/rotom/default.nix
+++ systems/rotom/default.nix
@@ -18,9 +18,6 @@ with lib;
   users.users.slabity = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
-    openssh.authorizedKeys.keys = [
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC5235XOTxsFp/COvl8K67OO7adCk7vR/HZHRYFXZ8nnbZdQjRxWlnvHJXfHVeofFQ7iUeLCIX/4f/D5kAF+LKxBoDa5lkNMP4gDV5GX8XFCarnxnQbJCyXiRg0dIX/3POZLdeQNF82XaYjCN3oTjEzNrxZH7Mj7/6rj9J+9sCIxeDR7UATgRO55XfUwDKNYQYFRMiJVMwtn8K7IBZuM2GKO3FYtqrw3bOMFYApOtTmVwkRNyXJC3sisWQdDOzJ4I9wzyGFVLdTvEPoUygAKs0K/uit0WKxwxTD9XiPtBZww8sw0locdIaZDkSd12HUfEmp1j/PghiIWPZnfU0mHIc63uKJYg3DDBCU5SOYH3wCge3zkAhgWhpMXvyUiUe1Qd4SQepRVkL7YQZWkXdUNTKg1gIJ/yGYIYAdZoIu+nNJYE0JCObcN39HOt5XGYW5FbnMzv73OenrCM7y4Z8ac7K8GLiksWiN4MtkbKga9/uVH8E+j8goMjDBuokhZ5Kb6OuqRlYryRukiOiZ6Hf5g+VcUEojuAyWh3+Xodjlneqe/hbigYHe2v+AS/Thf8h/G84KSzDmoIsCFg7ISgBjYKRt7aAsIX92yNAs2+qjeoOip7l73WnvpzKGE5zK/pKU4j6+W253VjBXxw8rbq8mVEvha8CIooWP+fd3VtDG3k37/Q== slabity@nixos"
-    ];
   };
 
   home-manager = {
