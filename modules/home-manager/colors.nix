5a7d73bae2711dcf3f460d09d5ddaefcdd6c7aea
Tyler Slabinski <tslabinski@slabity.net>
2024-08-05 15:01:33 -0400

    Add colors module

---
+++ modules/home-manager/colors.nix
@@ -0,0 +1,24 @@
+{ config, lib, pkgs, flake, ... }:
+with lib;
+{
+  colors = {
+    primary.background = "#151017";
+    primary.foreground = "#F5E1FF";
+    normal.black = "#3B2B42";
+    normal.red = "#CC396A";
+    normal.green = "#00919E";
+    normal.yellow = "#CC4759";
+    normal.blue = "#7B42CC";
+    normal.magenta = "#B72AD9";
+    normal.cyan = "#1C7ACC";
+    normal.white = "#9883A3";
+    bright.black = "#5A4b62";
+    bright.red = "#F288AB";
+    bright.green = "#6CBDC5";
+    bright.yellow = "#EB7181";
+    bright.blue = "#B295DB";
+    bright.magenta = "#D899E8";
+    bright.cyan = "#9AC1E4";
+    bright.white = "#EAD8FF";
+  };
+}
--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -2,6 +2,7 @@
 with lib;
 {
   imports = [
+    ./colors.nix
     ./neovim.nix
     ./zsh.nix
     ./workstation
